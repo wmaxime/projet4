1fb7f7c95d12b3438ea9720708376d375c87362e
wmaxime <adm-linux@EPV2106X031P.epv.net>
2023-07-23 00:15:31 +0200

    fix deployment scripts and restauration crash

--- truffle/migrations/1_deploy_Project.js
+++ truffle/migrations/1_deploy_Project.js
@@ -1,12 +1,13 @@
 const EVCT = artifacts.require("EVCT");
 const Vault = artifacts.require("Vault");
-const Staking = artifacts.require("Staking")
+const Staking = artifacts.require("Staking");
 
 module.exports = async function (deployer) {
-    deployer.deploy(EVCT);
-    deployer.deploy(Vault);
+    await deployer.deploy(EVCT);
+    await deployer.deploy(Vault);
 
     const token = await EVCT.deployed();
     const vault = await Vault.deployed();
+    console.log("ADRESS VAULT ============== " + vault.address);
     await deployer.deploy(Staking, token.address, vault.address);
 };
--- truffle/migrations/4_deploy_Staking.js
+++ truffle/migrations/4_deploy_Staking.js
@@ -1,5 +1,5 @@
 const Staking = artifacts.require("Staking");
 
-module.exports = function (deployer) {
-  deployer.deploy(Staking);
+module.exports = async function (deployer) {
+  await deployer.deploy(Staking, "0x9b10E3FE54f07a8c00292Dda2827E823D7ec79A3", "0x4C4f8f016Ad8acFdbA06c5B91D6C1a7Fe86DFE50");
 };
